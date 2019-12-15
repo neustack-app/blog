a3ad029e72cb89c6bd11ad85bc192bd1fd5fcde9
Surya J <suryaj@gmail.com>
2019-12-14 20:39:56 -0800

    add origin

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,8 @@ if [[ $TRAVIS_BRANCH == 'dev' ]] ; then
   git remote rm origin
   # We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  git push --force --quiet "https://${github_user}:${github_key}@${blog_target}" origin gh-pages > /dev/null 2>&1
+  git remote add origin "https://${github_user}:${github_key}@${blog_target}" > /dev/null 2>&1
+  git push origin gh-pages --quiet --force
 else
   echo 'Invalid branch. You can only deploy from master.'
   exit 1
